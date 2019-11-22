9efb38aae7ec685976ef0403c320456c06cb9a24
André <andrer@ppedv.de>
2019-11-22 15:31:17 +0100

    Bank WCF Client

---
+++ WCFBank/WCFBank/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WCFBank
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var client = new ServiceReference1.BLZServicePortTypeClient("BLZServiceSOAP12port_http");
+            var bank = client.getBank("51050015");
+            MessageBox.Show(bank.bezeichnung);
+        }
+    }
+}
